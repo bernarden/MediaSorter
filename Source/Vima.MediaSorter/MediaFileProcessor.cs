a71944a26356df83a11124c957d3c1fb9d1fc9a1
Victor Usoltsev <bernarden@mail.ru>
2022-11-21 00:50:30 +1300

    Stops creation of new folders if description is present.

--- Source/Vima.MediaSorter/MediaFileProcessor.cs
+++ Source/Vima.MediaSorter/MediaFileProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using Vima.MediaSorter.Domain;
@@ -9,11 +10,14 @@ namespace Vima.MediaSorter;
 
 public class MediaFileProcessor
 {
+    public static readonly string FolderNameFormat = "yyyy_MM_dd -";
     public static readonly List<string> ImageExtensions = new() { ".JPG" };
     public static readonly List<string> VideoExtensions = new() { ".MP4" };
     private readonly IList<DuplicateFile> _duplicateFiles;
     private readonly string _sourceDirectory;
-
+    private readonly Dictionary<DateTime, string> _dateToExistingDirectoryMapping = new();
+    private readonly List<string> _skippedExistingDirectories = new();
+    
     public MediaFileProcessor(string sourceDirectory)
     {
         _sourceDirectory = sourceDirectory;
@@ -32,7 +36,37 @@ public class MediaFileProcessor
 
     private List<MediaFile> IdentifyMediaFiles()
     {
-        Console.Write("Identifying your media... ");
+        Console.WriteLine("Identifying your media... ");
+
+        // Find previously used media directories.
+        string[] directoryPaths = Directory.GetDirectories(_sourceDirectory);
+        foreach (string directoryPath in directoryPaths)
+        {
+            string directoryName = new DirectoryInfo(directoryPath).Name;
+            if (directoryName.Length < FolderNameFormat.Length)
+            {
+                Console.WriteLine($"\tWarning: Skipping media directory: '{directoryName}'.");
+                _skippedExistingDirectories.Add(directoryName);
+                continue;
+            }
+
+            string directoryNameBeginning = directoryName[..FolderNameFormat.Length];
+            if (!DateTime.TryParseExact(directoryNameBeginning, FolderNameFormat,
+                    CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            {
+                Console.WriteLine($"\tWarning: Skipping media directory: '{directoryName}'.");
+                _skippedExistingDirectories.Add(directoryName);
+                continue;
+            }
+
+            if (_dateToExistingDirectoryMapping.TryGetValue(result.Date, out string? existingDirectoryName))
+            {
+                Console.WriteLine($"\tWarning: Multiple existing directories mapped to date: '${result.ToShortDateString()}'. Only '{existingDirectoryName}' is going to be used.");
+            }
+
+            _dateToExistingDirectoryMapping.Add(result.Date, directoryName);
+        }
+
         using ProgressBar progress = new();
         string[] filePaths = Directory.GetFiles(_sourceDirectory);
         List<MediaFile> mediaFiles = new();
@@ -98,8 +132,14 @@ public class MediaFileProcessor
 
     private void MoveFile(string filePath, DateTime createdDateTime)
     {
+        if (!_dateToExistingDirectoryMapping.TryGetValue(createdDateTime, out string? directoryName))
+        {
+            directoryName  = createdDateTime.ToString("yyyy_MM_dd -");
+        }
+
+        string destinationFolderPath = Path.Combine(_sourceDirectory, directoryName);
         (FileMovingHelper.MoveStatus status, string? destinationPath) =
-            FileMovingHelper.MoveFile(_sourceDirectory, filePath, createdDateTime);
+            FileMovingHelper.MoveFile(filePath, destinationFolderPath);
         if (status == FileMovingHelper.MoveStatus.Duplicate)
         {
             DuplicateFile duplicateFile = new(filePath, destinationPath ?? string.Empty);
