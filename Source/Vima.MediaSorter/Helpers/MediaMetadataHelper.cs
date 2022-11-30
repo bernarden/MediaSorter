40847c7cef8960f90352f6ead207107b15416e68
Victor Usoltsev <bernarden@mail.ru>
2022-11-30 21:50:13 +1300

    Gets creation date from image's file name.

--- Source/Vima.MediaSorter/Helpers/MediaMetadataHelper.cs
+++ Source/Vima.MediaSorter/Helpers/MediaMetadataHelper.cs
@@ -25,6 +25,9 @@ public class MediaMetadataHelper
 
     public static DateTime? GetImageDatetimeCreatedFromMetadata(string filePath)
     {
+        if (TryGetDateTimeFromName(filePath, out DateTime? dateTimeFromName))
+            return dateTimeFromName;
+
         using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
         IReadOnlyList<MetadataExtractor.Directory> directories =
             JpegMetadataReader.ReadMetadata(fs, new[] { new ExifReader() });
@@ -40,17 +43,8 @@ public class MediaMetadataHelper
 
     public static DateTime? GetVideoCreatedDateTimeFromName(string filePath)
     {
-        // Try to get creation date from file name.
-        Regex rgx = new(@"(?<year>[12]\d{3})(?<month>0[1-9]|1[0-2])(?<day>[012]\d|3[01])");
-        Match mat = rgx.Match(filePath);
-        if (mat.Success)
-        {
-            GroupCollection groups = mat.Groups;
-            int year = int.Parse(groups["year"].Value);
-            int month = int.Parse(groups["month"].Value);
-            int day = int.Parse(groups["day"].Value);
-            return new DateTime(year, month, day);
-        }
+        if (TryGetDateTimeFromName(filePath, out DateTime? dateTimeFromName))
+            return dateTimeFromName;
 
         // Try to get creation date from metadata tag.
         using FileStream fs = new(filePath, FileMode.Open, FileAccess.Read);
@@ -59,12 +53,35 @@ public class MediaMetadataHelper
             .OfType<QuickTimeMovieHeaderDirectory>()
             .FirstOrDefault();
         if (subIfdDirectory == null) return null;
-        if (subIfdDirectory.TryGetDateTime(QuickTimeMovieHeaderDirectory.TagCreated,
-                out DateTime tagCreatedUtcResult))
+        if (subIfdDirectory.TryGetDateTime(
+                QuickTimeMovieHeaderDirectory.TagCreated, out DateTime tagCreatedUtcResult) &&
+            !tagCreatedUtcResult.Equals(new DateTime(1904, 01, 01, 0, 0, 0)))
         {
             return tagCreatedUtcResult.ToLocalTime();
         }
 
         return null;
+    }
+
+    private static bool TryGetDateTimeFromName(string filePath, out DateTime? dateTimeFromName)
+    {
+        // Try to get creation date from file name.
+        string fileName = Path.GetFileName(filePath);
+        Regex rgx = new(@"(?<year>[12]\d{3})(?<month>0[1-9]|1[0-2])(?<day>[012]\d|3[01])");
+        Match mat = rgx.Match(fileName);
+        if (mat.Success)
+        {
+            GroupCollection groups = mat.Groups;
+            int year = int.Parse(groups["year"].Value);
+            int month = int.Parse(groups["month"].Value);
+            int day = int.Parse(groups["day"].Value);
+            {
+                dateTimeFromName = new DateTime(year, month, day);
+                return true;
+            }
+        }
+
+        dateTimeFromName = null;
+        return false;
     }
 }
