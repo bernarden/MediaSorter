d32f6683308a45768c9b15b02040e249bf171b6c
Victor Usoltsev <Bernarden@mail.ru>
2024-05-24 00:36:36 +1200

    Moves version to csproj file.

--- Source/Vima.MediaSorter/Program.cs
+++ Source/Vima.MediaSorter/Program.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace Vima.MediaSorter;
 
@@ -6,6 +9,10 @@ public class Program
 {
     public static void Main()
     {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+        Console.WriteLine($"Vima MediaSorter v{fileVersionInfo.FileVersion}");
+
         string sourceDirectory = Directory.GetCurrentDirectory();
         MediaFileProcessor processor = new(sourceDirectory);
         processor.Process();
