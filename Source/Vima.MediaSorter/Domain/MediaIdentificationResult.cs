7891e8a38afe176ea0fb93770345d860279dbc7b
Bernarden <12289537+bernarden@users.noreply.github.com>
2026-01-07 01:37:14 +1300

    Moves media identifying and sorting logic from MediaFileProcessor into corresponding services.

---
+++ Source/Vima.MediaSorter/Domain/MediaIdentificationResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vima.MediaSorter.Domain;
+
+public class MediaIdentificationResult
+{
+    public List<MediaFile> MediaFiles { get; init; } = new();
+    public IReadOnlyDictionary<DateTime, string> ExistingDirectoryMapping { get; init; } = new Dictionary<DateTime, string>();
+    public IReadOnlyList<string> IgnoredFiles { get; init; } = new List<string>();
+}
