87bfe82545270d32df299a7e97ed765e2b36a136
Bernarden <bernarden@users.noreply.github.com>
2026-01-24 00:58:24 +1300

    Adds target sub folder for media file. Moves cr3 files to raw sub folder.

--- Source/Vima.MediaSorter/Domain/MediaFile.cs
+++ Source/Vima.MediaSorter/Domain/MediaFile.cs
@@ -7,9 +7,8 @@ public class MediaFile(string filePath)
     public string FilePath { get; } = filePath;
     public List<string> RelatedFiles { get; } = [];
     public CreatedOn? CreatedOn { get; private set; }
+    public string? TargetSubFolder { get; private set; }
 
-    public void SetCreatedOn(CreatedOn? createdOn)
-    {
-        CreatedOn = createdOn;
-    }
+    public void SetCreatedOn(CreatedOn? createdOn) => CreatedOn = createdOn;
+    public void SetTargetSubFolder(string subFolder) => TargetSubFolder = subFolder;
 }
