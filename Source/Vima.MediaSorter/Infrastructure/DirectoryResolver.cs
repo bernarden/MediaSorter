7a77625a9cde46a03928bdbe56b5aa0bc0a75b96
Bernarden <bernarden@users.noreply.github.com>
2026-01-25 00:30:08 +1300

    Refactors media sorting service.

---
+++ Source/Vima.MediaSorter/Infrastructure/DirectoryResolver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Vima.MediaSorter.Domain;
+
+namespace Vima.MediaSorter.Infrastructure;
+
+public interface IDirectoryResolver
+{
+    DateTime? GetDateFromDirectoryName(string directoryName);
+    string GetTargetFolderPath(
+        DateTime date,
+        string? subFolder,
+        IDictionary<DateTime, string> dateToExistingDirectoryMapping
+    );
+}
+
+public class DirectoryResolver(MediaSorterSettings settings) : IDirectoryResolver
+{
+    public DateTime? GetDateFromDirectoryName(string directoryName)
+    {
+        if (directoryName.Length < settings.FolderNameFormat.Length)
+            return null;
+
+        string directoryNameBeginning = directoryName.Substring(0, settings.FolderNameFormat.Length);
+        return DateTime.TryParseExact(
+            directoryNameBeginning,
+            settings.FolderNameFormat,
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out DateTime result
+        )
+            ? result
+            : null;
+    }
+
+    public string GetTargetFolderPath(
+        DateTime date,
+        string? subFolder,
+        IDictionary<DateTime, string> dateToExistingDirectoryMapping
+    )
+    {
+        if (!dateToExistingDirectoryMapping.TryGetValue(date.Date, out var folderName))
+            folderName = date.ToString(settings.FolderNameFormat, CultureInfo.InvariantCulture);
+
+        var path = Path.Combine(settings.Directory, folderName);
+        return string.IsNullOrEmpty(subFolder) ? path : Path.Combine(path, subFolder);
+    }
+}
--- Source/Vima.MediaSorter/Services/MediaSortingService.cs
+++ Source/Vima.MediaSorter/Services/MediaSortingService.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Vima.MediaSorter.Domain;
@@ -19,53 +17,28 @@ public interface IMediaSortingService
         IDictionary<DateTime, string> dateToExistingDirectoryMapping
     );
 }
-
-public class MediaSortingService(IFileMover fileMover, MediaSorterSettings settings) : IMediaSortingService
+public class MediaSortingService(
+    IFileMover fileMover,
+    IDirectoryResolver directoryResolver,
+    ITimeZoneAdjustingService timeZoneAdjuster,
+    MediaSorterSettings settings) : IMediaSortingService
 {
     public List<DuplicateFile> Sort(
         IReadOnlyCollection<MediaFile> files,
-        IDictionary<DateTime, string> dateToExistingDirectoryMapping
-    )
+        IDictionary<DateTime, string> dateToExistingDirectoryMapping)
     {
-        var duplicates = new ConcurrentBag<DuplicateFile>();
-        if (files == null || files.Count == 0) return duplicates.ToList();
+        if (files == null || files.Count == 0) return [];
 
-        var mediaFilesToApplyOffset = files.Where(x => x.CreatedOn != null && x.CreatedOn.Source == CreatedOnSource.MetadataUtc).ToList();
-        if (mediaFilesToApplyOffset.Any())
-        {
-            TimeSpan utcOffset = ConsoleHelper.GetVideoUtcOffsetFromUser();
-            foreach (var mf in mediaFilesToApplyOffset)
-            {
-                mf.SetCreatedOn(new CreatedOn(mf.CreatedOn!.Date + utcOffset, CreatedOnSource.MetadataLocal));
-            }
-        }
+        timeZoneAdjuster.ApplyOffsetsIfNeeded(files);
 
         Console.Write("Sorting your media... ");
         using ProgressBar progress = new();
+        var duplicates = new ConcurrentBag<DuplicateFile>();
         var stepLogs = new ConcurrentBag<string>();
         int processedFileCounter = 0;
         Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = 25 }, file =>
         {
-            try
-            {
-                if (file.CreatedOn == null)
-                {
-                    stepLogs.Add($"\tWarning: No creation date detected: {file.FilePath}");
-                    return;
-                }
-
-                MoveFile(file.FilePath, file.CreatedOn.Date, file.TargetSubFolder, dateToExistingDirectoryMapping, duplicates);
-
-                foreach (string relatedFilePath in file.RelatedFiles)
-                {
-                    MoveFile(relatedFilePath, file.CreatedOn.Date, file.TargetSubFolder, dateToExistingDirectoryMapping, duplicates);
-                }
-            }
-            catch (Exception e)
-            {
-                stepLogs.Add($"\tWarning: Failed to sort: {file.FilePath}. Error: {e.Message}");
-            }
-
+            ProcessFile(file, dateToExistingDirectoryMapping, duplicates, stepLogs);
             Interlocked.Increment(ref processedFileCounter);
             progress.Report((double)processedFileCounter / files.Count);
         });
@@ -77,48 +50,44 @@ public class MediaSortingService(IFileMover fileMover, MediaSorterSettings setti
         return [.. duplicates];
     }
 
-    private void MoveFile(string filePath, DateTime createdDateTime, string? targetSubFolder, IDictionary<DateTime, string> dateToExistingDirectoryMapping, ConcurrentBag<DuplicateFile> duplicates)
+    private void ProcessFile(
+        MediaFile file,
+        IDictionary<DateTime, string> dateToExistingDirectoryMapping,
+        ConcurrentBag<DuplicateFile> duplicates,
+        ConcurrentBag<string> stepLogs)
     {
-        if (!dateToExistingDirectoryMapping.TryGetValue(createdDateTime.Date, out string? targetDirectoryName))
+        if (file.CreatedOn == null)
         {
-            targetDirectoryName = createdDateTime.ToString(settings.FolderNameFormat, CultureInfo.InvariantCulture);
+            stepLogs.Add($"  Warning: No creation date detected: {Path.GetRelativePath(settings.Directory, file.FilePath)}");
+            return;
         }
 
-        string targetFileName = Path.GetFileName(filePath);
+        var targetFolderPath = directoryResolver.GetTargetFolderPath(
+            file.CreatedOn.Date, file.TargetSubFolder, dateToExistingDirectoryMapping);
 
-        string targetFolderPath = Path.Combine(settings.Directory, targetDirectoryName);
-        if (!string.IsNullOrEmpty(targetSubFolder))
+        foreach (var filePath in (string[])[file.FilePath, .. file.RelatedFiles])
         {
-            targetFolderPath = Path.Combine(targetFolderPath, targetSubFolder);
-        }
+            try
+            {
+                if (IsAlreadySorted(filePath, targetFolderPath)) return;
 
-        if (filePath.StartsWith(targetFolderPath, StringComparison.OrdinalIgnoreCase)) return;
+                var (status, destinationPath) = fileMover.Move(filePath, targetFolderPath, Path.GetFileName(filePath));
+                if (status == MoveStatus.Duplicate)
+                    duplicates.Add(new DuplicateFile(filePath, destinationPath ?? string.Empty));
+            }
 
-        // Don't move files that have been previously (manually?) put into a dated folder.
-        if (IsAlreadySorted(filePath)) return;
-
-        var (status, destinationPath) = fileMover.Move(filePath, targetFolderPath, targetFileName);
-        if (status == MoveStatus.Duplicate)
-        {
-            duplicates.Add(new DuplicateFile(filePath, destinationPath ?? string.Empty));
+            catch (Exception e)
+            {
+                stepLogs.Add($"  Warning: Failed to sort: {Path.GetRelativePath(settings.Directory, file.FilePath)}. Error: {e.Message}");
+            }
         }
     }
 
-    private bool IsAlreadySorted(string filePath)
+    private bool IsAlreadySorted(string filePath, string targetFolderPath)
     {
         var parent = Directory.GetParent(filePath);
-        return GetDirectoryDateFromPath(parent?.Name ?? "") != null ||
-            GetDirectoryDateFromPath(parent?.Parent?.Name ?? "") != null;
-    }
-
-    private DateTime? GetDirectoryDateFromPath(string directoryName)
-    {
-        if (directoryName.Length < settings.FolderNameFormat.Length) return null;
-
-        string directoryNameBeginning = directoryName.Substring(0, settings.FolderNameFormat.Length);
-        return DateTime.TryParseExact(directoryNameBeginning, settings.FolderNameFormat,
-            CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result)
-            ? result
-            : null;
+        return filePath.StartsWith(targetFolderPath, StringComparison.OrdinalIgnoreCase) ||
+            directoryResolver.GetDateFromDirectoryName(parent?.Name ?? "") != null ||
+            directoryResolver.GetDateFromDirectoryName(parent?.Parent?.Name ?? "") != null;
     }
 }
