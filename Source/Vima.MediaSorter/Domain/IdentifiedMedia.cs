1574aa6290a1df55756fce5b094f13e0854d5d7f
Bernarden <12289537+bernarden@users.noreply.github.com>
2026-01-26 17:56:06 +1300

    Stores media files without created on date in UndatedMediaFiles list.

--- Source/Vima.MediaSorter/Domain/IdentifiedMedia.cs
+++ Source/Vima.MediaSorter/Domain/IdentifiedMedia.cs
@@ -5,5 +5,6 @@ namespace Vima.MediaSorter.Domain;
 public class IdentifiedMedia
 {
     public IReadOnlyList<MediaFile> MediaFiles { get; init; } = new List<MediaFile>();
+    public IReadOnlyList<MediaFile> UndatedMediaFiles { get; init; } = new List<MediaFile>();
     public IReadOnlyList<string> IgnoredFiles { get; init; } = new List<string>();
 }
