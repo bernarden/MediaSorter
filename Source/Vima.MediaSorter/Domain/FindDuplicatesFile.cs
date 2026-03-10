45105d1e1ee078ba32f88edc5da854869f3ebdef
Bernarden <12289537+bernarden@users.noreply.github.com>
2026-03-11 00:44:37 +1300

    Updates FindDuplicatesProcessor to save and load hashes when possible.

---
+++ Source/Vima.MediaSorter/Domain/FindDuplicatesFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Vima.MediaSorter.Domain;
+
+public class FindDuplicatesFile(
+    string Path,
+    string BinaryHash,
+    ulong AverageHash,
+    ulong DifferenceHash,
+    ulong PerceptualHash,
+    long Size,
+    int Width,
+    int Height,
+    DateTime LastModified
+)
+{
+    public string Path { get; } = Path;
+    public string BinaryHash { get; } = BinaryHash;
+    public ulong AverageHash { get; } = AverageHash;
+    public ulong DifferenceHash { get; } = DifferenceHash;
+    public ulong PerceptualHash { get; } = PerceptualHash;
+    public long Size { get; } = Size;
+    public int Width { get; } = Width;
+    public int Height { get; } = Height;
+    public DateTime LastModified { get; } = LastModified;
+}
---
+++ Source/Vima.MediaSorter/Domain/SourceGenerationContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+using Vima.MediaSorter.Domain;
+
+namespace Vima.MediaSorter.Processors;
+
+[JsonSerializable(typeof(FindDuplicatesFile))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
